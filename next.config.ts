a08ab4e3f0a6c3e40ff44183e80e05c7cafd4942
uuigfhj <mw4492997@gmail.com>
2026-01-31 21:58:28 +0300

    Force stable build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
 const nextConfig: NextConfig = {
   images: {
@@ -22,13 +19,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
+  transpilePackages: ['@netlify/plugin-nextjs'],
 };
 
 export default nextConfig;
