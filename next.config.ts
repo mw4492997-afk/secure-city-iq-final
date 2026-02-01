46545dbe765d0606e8ef24b0958e33b90687a386
uuigfhj <mw4492997@gmail.com>
2026-02-01 11:25:52 +0300

    UI Restored: Matching repository video and image assets

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   transpilePackages: ['@netlify/plugin-nextjs'],
 };
 
